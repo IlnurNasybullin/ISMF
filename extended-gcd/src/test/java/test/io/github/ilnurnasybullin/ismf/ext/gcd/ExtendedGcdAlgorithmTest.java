af2f0f47905d858b319a3da78ccf2322bbe88f78
Ilnur Nasybullin <ilnur.nasybullin2000@gmail.com>
2022-09-05 11:14:15 +0300

    simplify test data generation

--- extended-gcd/src/test/java/test/io/github/ilnurnasybullin/ismf/ext/gcd/ExtendedGcdAlgorithmTest.java
+++ extended-gcd/src/test/java/test/io/github/ilnurnasybullin/ismf/ext/gcd/ExtendedGcdAlgorithmTest.java
@@ -2,17 +2,25 @@ package test.io.github.ilnurnasybullin.ismf.ext.gcd;
 
 import io.github.ilnurnasybullin.ismf.ext.gcd.ExtendedGcdAlgorithm;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ExtendedGcdAlgorithmTest {
 
     @ParameterizedTest
-    @MethodSource("testIntCalculateDataSource")
+    @CsvSource(textBlock = """
+            # a,    b,      gcd
+            72,     25,     1,
+            25,     1,      1,
+            1,      25,     1,
+            10,     0,      10,
+            0,      -15,    15,
+            80,     -25,    5,
+            -25,    4,      1,
+            -35,    -60,    5,
+            19,     9,      1
+            """)
     public void testIntCalculate(int a, int b, int gcd) {
         var extendedGcd = new ExtendedGcdAlgorithm().calculate(a, b);
         assertThat(extendedGcd.gcd())
@@ -25,19 +33,4 @@ public class ExtendedGcdAlgorithmTest {
                 .isEqualTo(gcd);
 
     }
-
-    public static Stream<Arguments> testIntCalculateDataSource() {
-        return Stream.of(
-                Arguments.of(72, 25, 1),
-                Arguments.of(25, 1, 1),
-                Arguments.of(1, 25, 1),
-                Arguments.of(10, 0, 10),
-                Arguments.of(0, -15, 15),
-                Arguments.of(80, -25, 5),
-                Arguments.of(-25, 4, 1),
-                Arguments.of(-35, -60, 5),
-                Arguments.of(19, 9, 1)
-        );
-    }
-
 }
