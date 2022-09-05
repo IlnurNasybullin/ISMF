595e71e1cc37f9a16cdfb3b39b737ea39fdb7f22
Ilnur Nasybullin <ilnur.nasybullin2000@gmail.com>
2022-09-05 12:27:28 +0300

    developed pow-mod

--- pow-mod/src/main/java/io/github/ilnurnasybullin/ismf/pow/mod/PowMod.java
+++ pow-mod/src/main/java/io/github/ilnurnasybullin/ismf/pow/mod/PowMod.java
@@ -1,8 +1,25 @@
 package io.github.ilnurnasybullin.ismf.pow.mod;
 
+import io.github.ilnurnasybullin.ismf.ext.gcd.ExtendedGcdAlgorithm;
+
 public class PowMod {
 
     public long powMod(long a, long b, long n) {
+        if (n < 0) {
+            throw new IllegalArgumentException(String.format("Modulo n = %s is negative!", n));
+        }
+
+        if (b < 0) {
+            var extendedGcd = new ExtendedGcdAlgorithm().calculate(a, n);
+            if (extendedGcd.gcd() != 1L) {
+                throw new IllegalArgumentException(
+                        String.format("Value a = %d is not invertible for modulo n = %d", a, n)
+                );
+            }
+
+            return powMod(extendedGcd.bezoutCoefficients().x(), Math.negateExact(b), n);
+        }
+
         var c = 1L;
         while (b != 0) {
             if (isEven(b)){
--- pow-mod/src/main/java/module-info.java
+++ pow-mod/src/main/java/module-info.java
@@ -1,3 +1,5 @@
 module io.github.ilnurnasybullin.ismf.pow.mod {
+    requires io.github.ilnurnasybullin.ismf.ext.gcd;
+
     exports io.github.ilnurnasybullin.ismf.pow.mod;
 }
